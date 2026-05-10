dcbb38ca941ec889d8ef66cae9fcd7754ed5a232
NguyenVietQuan2004 <nguyenvietquan.30032004@gmail.com>
2026-05-10 14:45:25 +0700

    upload

--- config/env.ts
+++ config/env.ts
@@ -9,5 +9,5 @@ export const ENV_CONFIG = {
 
   APP_URL: ENV === "dev" ? process.env.NEXT_PUBLIC_APP_URL_DEV : process.env.NEXT_PUBLIC_APP_URL_PROD,
   API_URL_STORAGE:
-    ENV === "dev" ? process.env.NEXT_PUBLIC_API_URL_STORAGE_DEV : process.env.NEXT_PUBLIC_API_URL_STORAGE_pro,
+    ENV === "dev" ? process.env.NEXT_PUBLIC_API_URL_STORAGE_DEV : process.env.NEXT_PUBLIC_API_URL_STORAGE_PROD,
 };
