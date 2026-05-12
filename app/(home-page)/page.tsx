d369698f1b1b1b9e8d8204f3d5f3f8f3ac57e07f
NguyenVietQuan2004 <nguyenvietquan.30032004@gmail.com>
2026-05-12 20:50:06 +0700

    upload

--- app/(home-page)/page.tsx
+++ app/(home-page)/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
     <HeaderLayout>
       <FooterLayout>
         <div className=" flex  items-center justify-center px-4 bg-background text-foreground transition-colors">
-          <div className="w-full max-w-2xl"> updatejkkk</div>
+          <div className="w-full max-w-2xl"> toi la con cun ngoan</div>
         </div>
       </FooterLayout>
     </HeaderLayout>
